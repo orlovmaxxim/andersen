a858af43dfe8ae32b905fd5176688901f7a43da9
Maxxim <orlov.maxxim@gmail.com>
2016-09-15 20:23:40 +0300

    add yandex map, add pictures

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,28 @@ $(document).ready(function(){
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev'
   });
+
+  ymaps.ready(init);
+  var myMap;
+
+  function init(){
+    myMap = new ymaps.Map("map", {
+      center: [45.068512, 39.033317],
+      zoom: 15,
+      controls: []
+    }, {
+      searchControlProvider: 'yandex#search'
+    });
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        // hintContent: 'Anderson mark',
+        // balloonContent: 'Это красивая метка'
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/map-marker.png',
+        iconImageSize: [95, 80],
+        iconImageOffset: [0, 0]
+      });
+    myMap.geoObjects.add(myPlacemark);
+  }
+
 });
