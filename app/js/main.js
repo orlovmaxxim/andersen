cf89b109f6ab5abd1b4570beb6061cb40c9c93a9
Max Orlov <orlov.maxxim@gmail.com>
2016-09-27 01:27:16 +0300

    add slick js

--- app/js/main.js
+++ app/js/main.js
@@ -24,6 +24,49 @@ $(document).ready(function(){
     spaceBetween: 10
   });
 
+
+  // slick slider
+
+
+  $('.slick-container-manager').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+  // yandex map
+
   ymaps.ready(init);
    var myMap;
 
