9b7cec4e9b248b253d161cc64d554cdf2109ad3d
Maxxim <orlov.maxxim@gmail.com>
2016-10-20 19:04:53 +0300

    change paths in img

--- app/js/modules/managerConsult.js
+++ app/js/modules/managerConsult.js
@@ -3,37 +3,37 @@ module.exports = {
     var managers = {
       firstManager : {
         name: "Ирины",
-        fot: "/img/manager/kupchinskaya_irina.jpg",
+        fot: "../img/manager/kupchinskaya_irina.jpg",
         aboutme: "В своей работе главным считаю искреннее и внимательное отношение к клиенту"
       },
 
       secondManager: {
         name: "Андрея",
-        fot: "/img/manager/bocharnikov_andrey.jpg",
+        fot: "img/manager/bocharnikov_andrey.jpg",
         aboutme: "Все вопросы клиента для меня важны, всегда открыт для диалога"
       },
 
       thirdManager: {
         name: "Светланы",
-        fot: "/img/manager/dikopoltseva_svetlana.jpg",
+        fot: "img/manager/dikopoltseva_svetlana.jpg",
         aboutme: "Умею слышать других, отношусь к работе с душой и любовью"
       },
 
       fourthManager: {
         name: "Станислава",
-        fot: "/img/manager/stanislav_shipilov.jpg",
+        fot: "img/manager/stanislav_shipilov.jpg",
         aboutme: "Ценю время людей, с которыми работаю"
       },
 
       fifthManager: {
         name: "Анастасии",
-        fot: "/img/manager/titarenko_anastasiya.jpg",
+        fot: "img/manager/titarenko_anastasiya.jpg",
         aboutme: "Процесс покупки квартиры должен быть удобным и понятным, с радостью помогу в этом"
       },
 
       sixthManager: {
         name: "Светланы",
-        fot: "/img/manager/titova_svetlana.jpg",
+        fot: "img/manager/titova_svetlana.jpg",
         aboutme: "Хорошо разбираюсь в деталях своей работы, при покупке квартиры важны все нюансы"
       }
     };
